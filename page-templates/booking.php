c2e16ccbba11b132d7ec7bcf7161f3112e1602e2
Akibur <akibur.choton@gmail.com>
2021-03-07 18:44:11 +0600

    update 645

--- page-templates/booking.php
+++ page-templates/booking.php
@@ -16,22 +16,22 @@ $container = get_theme_mod('understrap_container_type');
 
 <div class="wrapper page-wrapper">
 
-	<div id="primary">
+    <div id="primary">
 
-		<main class="site-main" id="main" role="main">
+        <main class="site-main" id="main" role="main">
 
 
             <section class="hero banner-generic short-banner">
                 <div class="hero-content">
                     <div class="banner-container">
-                        <div class="single-slider" style="background-image: url('<?php echo IMG.'/breakdown_banner.jpg' ?>');">    
+                        <div class="single-slider" style="background-image: url('<?php echo IMG . '/breakdown_banner.jpg' ?>');">
                             <div class="slider-content">
                                 <div class="container">
                                     <div class="info-holder">
-                                    
-                                        <h1 class="slider-title">Contact Us</h1>             
+
+                                        <h1 class="slider-title">Contact Us</h1>
                                         <h4>Our friendly team is here to help and always on hand.</h4>
-                                    
+
                                     </div>
                                 </div>
                             </div>
@@ -40,53 +40,61 @@ $container = get_theme_mod('understrap_container_type');
                 </div>
             </section> <!-- .banner-generic -->
 
-            
+            <section id="booking_form">
+                <!-- multistep form -->
+                <div class="container">
+                    <div class="cover-container">
+                        <div class="cover-cards">
+                            <div class="cover-card-seventy">
+                                <div class="card-item-header">
+                                    <p class="card-title">Bronze Breakdown Cover</p>
+                                </div>
+
+                                <div class="card-description">
+                                    <div class="card-paragraph">
+                                        <p>
+                                        Our cheapest breakdown cover. Includes your basic cover options such as a jump start and tyre change, as well as cover at home.
+                                        </p>
+                                    </div> 
+
+                                    <div class="card-list">
+                                        <ul>
+                                            <li> Road Assistance</li>
+                                            <li> Cover at Home</li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="card-details">
+                                        <p>
+                                            <span class="icon-detail"></span>
+                                            <span class="detail-text">See cover details</span>
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="cover-card-thirty">
+                                <div class="card-item-header">
+                                    <p class="card-price">£90 <span class="small-text">Per Year</span></p>
+                                </div>
+
+                                <div class="card-btn">
+                                    Choose this cover
+                                </div>
+                            
+                            </div>
+                        </div>
+
+                        <div class="basket-card">
+
+                        </div>
+                    </div>
+                </div>
+                
+            </section>
         </main><!-- #main -->
 
-</div><!-- #primary -->
-
-
-
-
-
-<section id="booking_form">
-    <!-- multistep form -->
-    <form id="msform">
-        <!-- progressbar -->
-        <ul id="progressbar">
-            <li class="active">Account Setup</li>
-            <li>Social Profiles</li>
-            <li>Personal Details</li>
-        </ul>
-        <!-- fieldsets -->
-        <fieldset>
-            <h2 class="fs-title">Create your account</h2>
-            <h3 class="fs-subtitle">This is step 1</h3>
-            <input type="text" name="email" placeholder="Email" />
-            <input type="password" name="pass" placeholder="Password" />
-            <input type="password" name="cpass" placeholder="Confirm Password" />
-            <input type="button" name="next" class="next action-button" value="Next" />
-        </fieldset>
-        <fieldset>
-            <h2 class="fs-title">Social Profiles</h2>
-            <h3 class="fs-subtitle">Your presence on the social network</h3>
-            <input type="text" name="twitter" placeholder="Twitter" />
-            <input type="text" name="facebook" placeholder="Facebook" />
-            <input type="text" name="gplus" placeholder="Google Plus" />
-            <input type="button" name="previous" class="previous action-button" value="Previous" />
-            <input type="button" name="next" class="next action-button" value="Next" />
-        </fieldset>
-        <fieldset>
-            <h2 class="fs-title">Personal Details</h2>
-            <h3 class="fs-subtitle">We will never sell it</h3>
-            <input type="text" name="fname" placeholder="First Name" />
-            <input type="text" name="lname" placeholder="Last Name" />
-            <input type="text" name="phone" placeholder="Phone" />
-            <textarea name="address" placeholder="Address"></textarea>
-            <input type="button" name="previous" class="previous action-button" value="Previous" />
-            <input type="submit" name="submit" class="submit action-button" value="Submit" />
-        </fieldset>
-    </form>
-</section>
+    </div><!-- #primary -->
 
 <?php get_footer();
